98136b7825de9d8dc77be1866b0b876835850432
Riyagrrm <120349265+Riyagrrm@users.noreply.github.com>
2023-06-19 16:37:36 +1000

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,7 @@
+username=localStorage.getItem("user_name")
+document.getElementById("user_name").innerHTML="Welcome "+user_name+"!"
+function addRoom()
+{
+    room_name=document.getElementById("room_name").value
+    
+}
